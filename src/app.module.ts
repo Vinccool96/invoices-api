4c99492c445fc54e3778a2a9bb93331a46860221
Vincent Girard <vinccool96@gmail.com>
2024-05-14 15:04:19 +0000

    Base app layout

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common"
 import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
+import { RevenuesModule } from "./revenues/revenues.module"
+import { CardsModule } from "./cards/cards.module"
+import { InvoicesModule } from "./invoices/invoices.module"
+import { UsersModule } from "./users/users.module"
 
 @Module({
-  imports: [],
+  imports: [RevenuesModule, CardsModule, InvoicesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
