20c4fc7634a6bc562e5deb947bc1ca53045ff34b
Vincent Girard <vinccool96@gmail.com>
2024-05-14 18:15:30 +0000

    Setup data validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from "@nestjs/core"
+import { ValidationPipe } from "@nestjs/common"
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 
 import { AppModule } from "./app.module"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      always: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+        enableCircularCheck: true,
+      },
+    }),
+  )
 
   const config = new DocumentBuilder()
     .setTitle("Invoices API")
